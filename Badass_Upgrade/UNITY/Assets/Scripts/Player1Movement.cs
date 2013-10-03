4ca46501971523c7b98e9bb482b2b8d1011658b5
casals90 <jordicasals90@gmail.com>
2013-10-03 17:39:19 +0200

    first commit
    proves commit

---
+++ Badass_Upgrade/UNITY/Assets/Scripts/Player1Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player1Movement : MonoBehaviour {
+	
+	float walkAcceleration = 5;
+	public GameObject cameraPlayer;
+	float jump = 20;
+	Vector2 horizontalMove;
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//transform.rotation = Quaternion.Euler(0,cameraPlayer.GetComponent(Player1Mouse).currentYRotation,0);
+		
+		rigidbody.AddRelativeForce(Input.GetAxis("Horizontal")*walkAcceleration,0,Input.GetAxis("Vertical")*walkAcceleration);
+	
+	}
+}
