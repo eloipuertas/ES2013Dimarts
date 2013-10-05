e831430dad114387c8bd58e16acf0369a8aa6cf5
Dabami <dabami7@gmail.com>
2013-10-05 14:42:28 +0200

    Script del HUD que muestra la información de la vida y el escudo.
    Tanto la vida como el escudo mostrados en el HUD son objetos del tipo
    GUIText que obtienen la información del script del personaje principal
    que la contenga (PE: MainCharacter).

---
+++ Badass_Upgrade/UNITY/Assets/Scripts/HUD.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HUD : MonoBehaviour {
+	
+	/*Tanto el GUIText como el MainCharacter necesitan ser asignados a los correspondientes objetos de la escena
+	 * vidaText: un objeto de tipo GUIText
+	 * escudoText: un objeto de tipo GUIText
+	 * robotProtagonista: un objeto que contenga el script MainCharacter 
+	 */
+	
+	public GUIText vidaText;
+	public GUIText escudoText;
+	
+	//El nombre del script que contiene la informacion del personaje podria ser distinto
+	public MainCharacter robotProtagonista;
+	
+	//The first method to be called
+	void Awake(){
+		
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+		vidaText.text = robotProtagonista.vida.ToString();
+		escudoText.text = robotProtagonista.escudo.ToString();
+		
+		//Los atributos vida y escudo del robotProtagonista son public float.
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
