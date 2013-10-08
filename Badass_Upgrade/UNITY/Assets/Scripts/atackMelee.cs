dd2f9cede2884aa7acc59b9415d5e7feef51d7b1
casals90 <jordicasals90@gmail.com>
2013-10-08 19:45:10 +0200

    atack melee
    #13 atack melee animation ok

---
+++ Badass_Upgrade/UNITY/Assets/Scripts/atackMelee.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class atackMelee : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		//animation.Stop();
+		//animation.PlayQueued("AmatureAction");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Input.GetKeyDown ("q")) {
+                 animation .Play();
+		} 
+	
+	}
+}
