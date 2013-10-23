136fba0439b001563316b2f5a6322f18ee5deb79
AlcoholNahMas <cucaverda@hotmail.com>
2013-10-23 23:29:38 +0200

    He canviat el cs dels barrils #153
    He modificat el codi dels barrils i ha estat comprovat per un programer
    del joc

--- Badass_Upgrade/DESIGN/Disseny de barrils explosius/barril.cs
+++ Badass_Upgrade/DESIGN/Disseny de barrils explosius/barril.cs
@@ -4,8 +4,11 @@ using System.Collections;
 public class barril : MonoBehaviour {
 	int vida=4;
 	public GameObject Barril;
+	public GameObject DestroyBarril;
+	
 	// Use this for initialization
 	void Start () {
+		DestroyBarril.SetActive(false);
 	
 	}
 	
@@ -15,10 +18,10 @@ public class barril : MonoBehaviour {
 	}
 	
 	void rebreTir (){
-		vida=vida-1;
-		if(vida==0){
-			Barril.setActive(false);
-			DestroyBarril.setActive(true);
+		vida -= 1; 
+		if(vida <= 0) { 
+			Barril.SetActive(false); 
+			DestroyBarril.SetActive(true);
 		}
 	}
 }
