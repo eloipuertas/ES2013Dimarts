3badb5b855608952c9c16bd55e35ce6d5241fe02
AlcoholNahMas <cucaverda@hotmail.com>
2013-10-23 22:20:30 +0200

    Barrils simples #153
    Aquests barrils simplemnt quan reben 4 dispars es canvien per el
    destruit

---
+++ Badass_Upgrade/DESIGN/Disseny de barrils explosius/barril.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class barril : MonoBehaviour {
+	int vida=4;
+	public GameObject Barril;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void rebreTir (){
+		vida=vida-1;
+		if(vida==0){
+			Barril.setActive(false);
+			DestroyBarril.setActive(true);
+		}
+	}
+}
