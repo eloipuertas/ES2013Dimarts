462d26302b7826e098d673e4dcba2b1356cd2052
camilopalacios <camilo.palacios.garzon@gmail.com>
2013-10-26 11:49:50 +0200

    Revert "#76 Funcionalidad del menu game over."
    This reverts commit 3b6c210e446a94406eaa96f3c1ba75a1d9e579b9.

--- Badass_Upgrade/UNITY/Assets/Scripts/HUD.cs
+++ Badass_Upgrade/UNITY/Assets/Scripts/HUD.cs
@@ -6,16 +6,20 @@ public class HUD : MonoBehaviour {
 	/*Tanto el GUIText como el MainCharacter necesitan ser asignados a los correspondientes objetos de la escena
 	 * vidaText: un objeto de tipo GUIText
 	 * escudoText: un objeto de tipo GUIText
+	 * enemiesCounterGUIText: un objeto de tipo GUIText
+	 * balasCargadorText: un objeto de tipo GUIText
+	 * balasTotalesText: un objeto de tipo GUIText
+	 * armaEquipada: un objeto que contiene el script con la informacion del arma
 	 * robotProtagonista: un objeto que contenga el script MainCharacter 
 	 */
-	
-	const int game_over = 3;
 	
 	public GUIText vidaText;
 	public GUIText escudoText;
 	public GUIText enemiesCounterGUIText;
+	public GUIText balasCargadorText;
+	public GUIText balasTotalesText;
 	
-	//El nombre del script que contiene la informacion del personaje podria ser distinto
+	public temporaryWeapon armaEquipada;
 	public MainCharacter robotProtagonista;
 	
 	//The first method to be called
@@ -29,7 +33,9 @@ public class HUD : MonoBehaviour {
 		vidaText.text = "Health: " + robotProtagonista.vida.ToString() + "%";
 		escudoText.text = "Shield: " + robotProtagonista.escudo.ToString() + "%";
 		enemiesCounterGUIText.text = "Remaining enemies: "; //+ robotProtagonista.enemies.ToString();
-		//Los atributos vida y escudo del robotProtagonista son public float.
+		
+		balasCargadorText.text = armaEquipada.balasCargador.ToString();
+		balasTotalesText.text = armaEquipada.balasTotales.ToString();
 		
 		
 	}
@@ -41,8 +47,10 @@ public class HUD : MonoBehaviour {
 		vidaText.text = "Health: " + robotProtagonista.vida.ToString() + "%";
 		escudoText.text = "Shield: " + robotProtagonista.escudo.ToString() + "%";
 		enemiesCounterGUIText.text = "Remaining enemies: "; //+ robotProtagonista.enemies.ToString();
-		if(robotProtagonista.vida <= 0){
-			Application.LoadLevel(game_over);
-		}
+		
+		
+		balasCargadorText.text = armaEquipada.balasCargador.ToString();
+		balasTotalesText.text = armaEquipada.balasTotales.ToString();
+		
 	}
 }
