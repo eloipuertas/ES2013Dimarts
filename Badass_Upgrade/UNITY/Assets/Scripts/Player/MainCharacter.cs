cf9bac2901f9429d8acac6fd25d80fd78a67092b
hiroCat <miki2189@gmail.com>
2013-10-27 03:35:02 +0100

    modificació de la llum
    modificació de la llanterna que no funcionava del tot correctament i de
    la intensitat , dels fluorescents , modificació també de la incorporació
    d'scripts al personatge al no estar colocats del tot correctament

--- Badass_Upgrade/UNITY/Assets/Scripts/Player/MainCharacter.cs
+++ Badass_Upgrade/UNITY/Assets/Scripts/Player/MainCharacter.cs
@@ -13,6 +13,9 @@ public class MainCharacter : MonoBehaviour {
 	
 	public bool vivo;
 	
+	// So en rebre un impacte
+	public AudioClip impactSound;
+	
 	//Atributs de les armes
 	int posWeapon;
 	int actualWeaponDamage;
@@ -33,6 +36,8 @@ public class MainCharacter : MonoBehaviour {
 	float maxScaleY = 1.0f;
 	//Per saber si esta ajupit
 	bool down;
+	
+	
 	
 
 	void Awake () {	
@@ -200,6 +205,7 @@ public class MainCharacter : MonoBehaviour {
 	
 	//Rebre dany de l'enemic
 	void rebreAtac(int dany) {
+		AudioSource.PlayClipAtPoint(impactSound,transform.position,0.15F);
 		if(escudo > 0) {
 			escudo -= dany;
 			if(escudo < 0)
