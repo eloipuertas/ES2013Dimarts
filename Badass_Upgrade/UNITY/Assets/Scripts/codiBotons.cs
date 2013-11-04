6c3eeff5221953534f7c48a00bc069fcf348e33c
AlcoholNahMas <cucaverda@hotmail.com>
2013-11-04 21:32:07 +0100

    #207 Crear logica dels botons
    Esta feta la relacio entre cada buto i plataforma

---
+++ Badass_Upgrade/UNITY/Assets/Scripts/codiBotons.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class codiBotons : MonoBehaviour {
+	
+	public GameObject Plataforma;
+
+	
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+	
+	
+	void activarBoto(){
+		Debug.Log("btooooooo");
+		Plataforma.SendMessage("setBotoActivat");
+	}
+		
+}
