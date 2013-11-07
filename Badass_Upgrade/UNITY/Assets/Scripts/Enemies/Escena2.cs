f0d03005da6e9453cf6147e828aba64598a625f7
MarcSanchez <marcsanchezmatas@gmail.com>
2013-11-07 01:13:30 +0100

    #220
    Implementació del sistema de waypoints pels enemics, Iluminacioó
    automatica de la escena passat un cert temps, i activació dels enemics.
    També activable col·lisionant amb el laser

---
+++ Badass_Upgrade/UNITY/Assets/Scripts/Enemies/Escena2.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Escena2 : MonoBehaviour {
+	
+	Light llum;
+
+	// Use this for initialization
+	
+	
+	void Start () {
+		
+		
+		StartCoroutine(Coroutine (10));
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	IEnumerator Coroutine (int temps) {
+		yield return new WaitForSeconds(temps);
+		engega_llums();
+	}
+	
+	
+	public void engega_llums(){
+		print ("llums!");
+		GameObject[] llums= GameObject.FindGameObjectsWithTag("llum2");
+		for (int i=0;i<llums.Length;i++){
+			llum=llums[i].GetComponent<Light>();
+			llum.enabled=true;
+		
+		}
+		
+		
+	}
+	
+	
+	public void activa_enemics(){
+		GameObject[] enemics= GameObject.FindGameObjectsWithTag("Enemy");
+		for (int i=0;i<enemics.Length;i++){
+			enemics[i].SendMessage("activar",SendMessageOptions.DontRequireReceiver);
+			
+		
+		}
+	
+	}
+	
+	
+}
---
+++ Badass_Upgrade/UNITY/Assets/Scripts/Enemies/LaserEscena2.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserEscena2 : MonoBehaviour {
+    private GameObject player;
+    private GameObject laser;
+	private GameObject cam;
+        
+        
+        void Awake(){
+            player = GameObject.FindGameObjectWithTag("Player");
+            laser = GameObject.FindGameObjectWithTag("Laser");
+			cam=GameObject.FindGameObjectWithTag("MainCamera");
+		
+        }
+        
+	
+        void OnTriggerStay(Collider other){
+            if(other.gameObject == player){
+                laser.SetActive(false);
+				cam.SendMessage("engega_llums");
+				cam.SendMessage("activa_enemics");
+                        
+            }
+        }
+
+
+
+}
