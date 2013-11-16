d8b989897ceeee97f6dc2eca282a1eefd5f303da
Xavier-RF <tirano_xavier117@hotmail.com>
2013-11-16 19:13:17 +0100

    Enemic invocador afegit #298
    Enemic que dispara missils perseguidors arreglat i afegit enemic que
    invoca altres enemics

--- Badass_Upgrade/UNITY/Assets/Scripts/Enemies/AI2Spawner.cs
+++ Badass_Upgrade/UNITY/Assets/Scripts/Enemies/AI2Spawner.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class AI2Homing : MonoBehaviour {
+public class AI2Spawner : MonoBehaviour {
     public Transform target;
 
     //Noms de les animacions:
@@ -14,18 +14,15 @@ public class AI2Homing : MonoBehaviour {
 
     //variables modificables segons la ia--------
 	public float vida=40;
-    public int moveSpeed=3;
-	public int rotationSpeed=2;
+    public int moveSpeed=10;
+	public int rotationSpeed=1;
     float Distance;
-    int dist_dmg=15;
-	int melee_dmg=25;
 
 	//----------------------------------
-    private int fireRate=3;
-    private int distancia_alerta=35;
-    private int distancia_perseguir=6;
-    private int distancia_melee=2;
-	private int distancia_disparar = 25;
+    private int fireRate=5;
+	private int directionChangeRate=5;
+    private int distancia_alerta=50;
+	private int distancia_disparar = 30;
 	private float escut =100, max_escut=100;
 	private int temps_recarga_escut=2;
 	private float regen_escut=20;
@@ -35,17 +32,14 @@ public class AI2Homing : MonoBehaviour {
     
     public Vector3 spawnPoint;
     public string state;
-    public float timerAtac,timerEscut;
+    public float timerAtac,timerEscut,timerDirection;
 	RaycastHit hit;
     GameObject hud;
     private Transform myTransform;
 	
 	
 	
-	public GameObject Homing_missile;
-	
-	
-	
+
     void Awake(){
         myTransform = transform;
         spawnPoint=new Vector3(transform.position.x,transform.position.y,transform.position.z);
@@ -68,7 +62,6 @@ public class AI2Homing : MonoBehaviour {
 		//regenerar_escut();
        	//myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
 		Distance=Vector3.Distance(target.position,transform.position);
-		
         Debug.DrawRay(transform.position, transform.forward);
 		//Debug.DrawLine(target.position, myTransform.position, Color.yellow);
                 
@@ -76,30 +69,13 @@ public class AI2Homing : MonoBehaviour {
         	state="away";
 			//Debug.Log("Enemic inactiu");
 			animation.CrossFade("ajupit");
-            //renderer.material.color=Color.blue;
-            //retorn al spawnpoint?
 		}else if(Distance<distancia_alerta && Distance>distancia_disparar){
 			if(state != "alerta"){
 				animation.CrossFade("activar");
 			}
 			state="alerta";
-			myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+			//myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
 			/*animation.CrossFade("activar");*/
-				
-        }else if(Distance<distancia_disparar && Distance>distancia_perseguir){
-			myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
-			state="shooting";
-            attack(dist_dmg,true);
-        }else if((Distance <=distancia_perseguir) && (Distance>distancia_melee)){
-			moveTo();
-            state = "walking";
-			animation.CrossFade("caminar");
-        }else if(Distance<distancia_melee ){
-			myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
-	        state="attack";
-	        //renderer.material.color=Color.red;
-			Debug.Log("Atacant a melee");
-	        attack(melee_dmg,false);
         }else{
 			if(state != "away"){
 				animation.CrossFade("desactivar");
@@ -107,33 +83,36 @@ public class AI2Homing : MonoBehaviour {
 			state="away";
 			//Debug.Log("Enemic inactiu");
 		}
+		if(Distance<distancia_disparar){
+			//myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
+			state="shooting";
+			if(Time.time>timerDirection){
+				moveTo();
+				timerDirection=Time.time+directionChangeRate;
+        	}
+            state = "walking";
+			animation.CrossFade("caminar");
+            spawn_enemy();
+        }
 	}
         
         
     void moveTo(){
-	    myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(target.position - myTransform.position), rotationSpeed * Time.deltaTime);
-	    myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
-	    //float mov= myTransform.position.y -(gravetat * Time.deltaTime);
-	    //myTransform.position.y =mov;
-	    //myTransform.position=new Vector3(myTransform.position.x,mov,myTransform.position.z);
+		//Vector3 randomDirection = new Vector3(0,Random.Range(-359, 359),Random.Range(-359, 359));
+		//myTransform.Rotate(randomDirection);
+	    //myTransform.rotation = Quaternion.Slerp(myTransform.rotation, Quaternion.LookRotation(randomDirection - myTransform.position), rotationSpeed * Time.deltaTime);
+	    //myTransform.position += myTransform.forward * moveSpeed * Time.deltaTime;
     }
 
-    private void attack(int dmg,bool ranged){
+    private void spawn_enemy(){
         if(Time.time>timerAtac){
-			if(ranged){
 				animation.CrossFade("disparar");
-				Debug.Log("Missile!");
+				Debug.Log("Spawn!");
 				Vector3 temp = myTransform.position;
-				temp.y = temp.y+3.0f;
+				temp.x = temp.x+Random.Range(2, 10);
+				temp.z = temp.z+Random.Range(2, 10);
 				timerAtac=Time.time+fireRate;
-				GameObject missile = (GameObject)Instantiate(Resources.Load("Homing_missile_1"),temp,myTransform.rotation);
-				//GameObject missile = (GameObject)Instantiate(Resources.Load("enemy"),temp,myTransform.rotation);
-			}else{
-				Debug.Log("Melee");
-				animation.CrossFade("melee");
-				disparar(distancia_disparar,melee_dmg);
-			}
-            timerAtac=Time.time+fireRate;
+				GameObject Spawned_Enemy = (GameObject)Instantiate(Resources.Load("enemy"),temp,myTransform.rotation);
         }
      }
 	
@@ -144,18 +123,6 @@ public class AI2Homing : MonoBehaviour {
 			Debug.Log("Enemigo muerto");
 			hud.SendMessage("enemyDeath");
 			Destroy(gameObject);
-		}
-	}
-	
-	private void disparar(int dis,int dmg){
-		if(Physics.Raycast(transform.position, (target.position- transform.position), out hit, dis)) {
-			Debug.DrawLine(target.position, transform.position, Color.green);
-			Debug.DrawRay(transform.position, transform.forward,Color.blue);
-			//print (hit.collider.gameObject.tag);
-			if(hit.collider.gameObject.tag == "Player") {
-				Debug.Log("ataco al player i li faig "+dmg+" punts de dany");
-				hit.transform.gameObject.SendMessage("rebreAtac",dmg);
-			}
 		}
 	}
 	
