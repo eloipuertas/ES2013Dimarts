fb82295ca18f150c99b3a305593dae81fe2679e2
AlcoholNahMas <cucaverda@hotmail.com>
2013-11-18 20:54:24 +0100

    Animar la palanca.
    He fet que quan s'activa te una animacio i quan es desactiva te una
    altre aixi el jugador pot veure si esta activat o desactivat. La
    implementacio de codi que he fet es pot aplicar tambe al boto per quan
    estigui arreglat, aixi es podra diferenciar quan estigui activat o
    desactivat, per aixo crec convenien guardar els canvis i penjarlos per a
    un futur utilitzarlos o es puguin aplicar en algun altre recurs.

--- Badass_Upgrade/UNITY/Assets/Scripts/codiBotons.cs
+++ Badass_Upgrade/UNITY/Assets/Scripts/codiBotons.cs
@@ -5,9 +5,11 @@ public class codiBotons : MonoBehaviour {
 	
 	public GameObject Plataforma;
 	public GameObject Porta;
+	public GameObject Buto_Palanca;
+	bool activat=false;
 	
 	void Start () {
-	
+		
 	}
 	
 
@@ -16,8 +18,31 @@ public class codiBotons : MonoBehaviour {
 	}
 	
 	
+	// Aixo inverteix la animacio de activar per tal de fer que la palanca pugui baixar i quedi desactivat, el
+	// que fa es de la animacio principar invertirla i la guardo en una nova que es diu "Desactivar"
+	
+	void invertirAnimacio(){
+		Debug.Log("INvertitttttt");
+		Buto_Palanca.animation["Desactivar"].speed = -1;
+    	Buto_Palanca.animation["Desactivar"].time = Buto_Palanca.animation["Desactivar"].length;
+
+	}
+	
+	
 	void activarBoto(){
 		Debug.Log("btooooooo");
+		
+		if (!activat){
+			Buto_Palanca.animation.CrossFade("Activar");
+			activat=true;
+		}else{
+			invertirAnimacio();
+			Buto_Palanca.animation.CrossFade("Desactivar");
+			invertirAnimacio();
+			activat=false;
+		
+		}
+		
 		if(Plataforma){
 			Plataforma.SendMessage("setBotoActivat");
 		}
