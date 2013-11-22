c1332f9f64147b48d8dfd2fd5fd5abc15795e17c
Alexander Avila <alex24.bcn@gmail.com>
2013-11-22 01:15:54 +0100

    Arreglados problemas #281
    Arreglado fondo rosa, y funcionalidad del boton restart.

--- Badass_Upgrade/UNITY/Assets/Scripts/GrupoE/gameOverMenuController.cs
+++ Badass_Upgrade/UNITY/Assets/Scripts/GrupoE/gameOverMenuController.cs
@@ -9,9 +9,12 @@ public class gameOverMenuController : MonoBehaviour {
 	
 	//Variables
 	public bool isNewGameButton = false;
-	public TextMesh count;
+	public GUIText count;
 	//public GameObject ob;
 	//public TextMesh mesh;
+	public Texture originalTexture;
+	public Texture hoverTexture;
+	public Vector3 scale;
 
 	
 	// Use this for initialization
@@ -39,12 +42,20 @@ public class gameOverMenuController : MonoBehaviour {
 
 	//This function is called when the mouse entered the GUIElement or Collider
 	public void OnMouseEnter(){
-		renderer.material.color = Color.blue;
+		originalTexture = guiTexture.texture;
+		scale = transform.localScale;
+		
+		if(isNewGameButton){
+			hoverTexture = Resources.Load("restartGame") as Texture;
+			guiTexture.texture = hoverTexture;
+			transform.localScale = new Vector3(0.01F, 0.01F, transform.localScale.z);
+		}
 	}
 	
 	//This function is called when the mouse is not any longer over the GUIElement or Collider
 	public void OnMouseExit(){
-		renderer.material.color = Color.white;
+		guiTexture.texture = originalTexture;
+		transform.localScale = scale;
 	}
 	
 	//This function is called when the user has released the mouse button
