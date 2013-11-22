e422af44535a81e20caf4cec68ed83f62fe59f90
Alexander Avila <alex24.bcn@gmail.com>
2013-11-22 19:07:08 +0100

    Funcionalidad boton volver, escena victoria
    Además de cambio texto al titulo escena gameover, y enlace del script
    exitdoor en escena nivel1 de pruebas.

--- Badass_Upgrade/UNITY/Assets/Scripts/GrupoE/victoryController.cs
+++ Badass_Upgrade/UNITY/Assets/Scripts/GrupoE/victoryController.cs
@@ -1,53 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class gameOverMenuController : MonoBehaviour {
+public class victoryController : MonoBehaviour {
 
 	//Constants
-	const int new_game = 1;
-	const int espera = 10; // Tiempo de espera hasta reiniciar nivel
-	
+	const int menu = 0;
 	//Variables
-	public bool isNewGameButton = false;
-	public GUIText count;
-	//public GameObject ob;
-	//public TextMesh mesh;
+	public bool isReturnMenu = false;
 	public Texture originalTexture;
 	public Texture hoverTexture;
 	public Vector3 scale;
-
 	
 	// Use this for initialization
 	void Start () {
-		Time.timeScale=1;
 		//Should the cursor be visible?
 		Screen.showCursor = true;
 		//The cursor will automatically be hidden, centered on view and made to never leave the view.
 		Screen.lockCursor = false;
-		// Muestra la cuenta atras
-		//ob = (GameObject)Instantiate(Resources.Load("Texto"));
-		//mesh = (TextMesh) ob.GetComponent("TextMesh");
-		//mesh.text = ""+espera;
-		count.text = ""+espera;
 	}
 	
-	private void Update()
-	{
-		if(Input.anyKey)
-			Application.LoadLevel(new_game);
-		if(Time.timeSinceLevelLoad > espera)
-			Application.LoadLevel(new_game);
-		else
-			count.text = ""+(10 - (int)Time.timeSinceLevelLoad);
+	// Update is called once per frame
+	void Update () {
 	}
-
+	
 	//This function is called when the mouse entered the GUIElement or Collider
 	public void OnMouseEnter(){
 		originalTexture = guiTexture.texture;
 		scale = transform.localScale;
 		
-		if(isNewGameButton){
-			hoverTexture = Resources.Load("restartGame") as Texture;
+		if(isReturnMenu){
+			hoverTexture = Resources.Load("return") as Texture;
 			guiTexture.texture = hoverTexture;
 			transform.localScale = new Vector3(0.01F, 0.01F, transform.localScale.z);
 		}
@@ -61,7 +43,7 @@ public class gameOverMenuController : MonoBehaviour {
 	
 	//This function is called when the user has released the mouse button
 	public void OnMouseUpAsButton(){
-		if(isNewGameButton)
-			Application.LoadLevel(new_game);
+		if(isReturnMenu)
+			Application.LoadLevel(menu);
 	}
 }
