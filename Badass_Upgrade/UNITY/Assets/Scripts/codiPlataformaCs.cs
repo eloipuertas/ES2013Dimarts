55515d5c3098de6fe2ced13b007dd8d6f4c09da5
D. Corto <thedavis@mac-thedavis.local>
2013-11-28 18:52:41 +0100

    Debug para la Issue #248
    Update del código para evitar que se queden las plataformas “atascadas”
    en los puntos de inicio o finales. Falta probar al 100% con todas las
    plataformas.

--- Badass_Upgrade/UNITY/Assets/Scripts/codiPlataformaCs.cs
+++ Badass_Upgrade/UNITY/Assets/Scripts/codiPlataformaCs.cs
@@ -2,11 +2,8 @@
 using System.Collections;
 
 public class codiPlataformaCs : MonoBehaviour {
-	
-	//public GameObject Plataforma;
-	//public GameObject Player;
-	public GameObject player;
-	bool direccio=false;
+
+	bool direccio=false; //true -> add position  / false -> remove position
 	bool plataformaQuieta = false;
 	public bool botoActivat=false;
 	
@@ -21,41 +18,19 @@ public class codiPlataformaCs : MonoBehaviour {
 	float PosicioInicialY;
 	float PosicioInicialZ;
 	bool paraPlataforma=false;
-
-	//public int distancia = 250;
-	//int cont = 0;
 	
-	bool itemBounceUp = false;
-
-//<<<<<<< HEAD
-	//void Start (bool x,bool y,bool z,int dist) {
-//=======
-	/*void Start (bool x,bool y,bool z,int dist) {
->>>>>>> origin/dev
-		eix_X = x;
-		eix_Y = y;
-		eix_Z = z;
-		distancia = dist;
-
-<<<<<<< HEAD
-	}
-=======
-	}*/
-//>>>>>>> origin/dev
+	string debugBoton = ""; //Only for debug outputs
+	string debugDireccion = ""; //Only for debug outputs
 	
 	void Start(){
 		PosicioInicialX = (this.transform.localPosition.x);  
 		PosicioInicialY = (this.transform.localPosition.y); 
 		PosicioInicialZ = (this.transform.localPosition.z); 
 		/*
-		PosicioInicialX = this.transform.position.x; 
-		PosicioInicialY = this.transform.position.y;
-		PosicioInicialZ = this.transform.position.z;
-		*/
 		Debug.Log(PosicioInicialX);
 		Debug.Log(PosicioInicialY);
 		Debug.Log(PosicioInicialZ);
-		
+		*/
 	}
 	
 	void Update(){
@@ -80,7 +55,8 @@ public class codiPlataformaCs : MonoBehaviour {
 						this.transform.Translate(0, 0, translation);
 					}
 					
-				}else{
+				} 
+				else {
 					if(eix_X){
 						this.transform.Translate(-translation, 0, 0);
 						
@@ -97,69 +73,99 @@ public class codiPlataformaCs : MonoBehaviour {
 				float x = this.transform.localPosition.x; 
 				float y = this.transform.localPosition.y; 
 				float z = (this.transform.localPosition.z); 
-				/*
-				float x = this.transform.position.x; 
-				float y = this.transform.position.y; 
-				float z = this.transform.position.z;
-				*/ 
-				Debug.Log(x);
-				Debug.Log(y);
-				Debug.Log(z);
 				
 				if(eix_X){
 					if (x<PosicioFinalX){
-						paraPlataforma=true;
-						StartCoroutine(paraDelay());
-						direccio = !this.direccio;
+						direccio = true; //para añadir siempre 
+						if(!paraPlataforma){
+							paraPlataforma=true;
+							StartCoroutine(paraDelay());
+						}
 					}
 					if (x>PosicioInicialX){
-						paraPlataforma=true;
-						StartCoroutine(paraDelay());
-						direccio = !this.direccio;
+						direccio = false; //para reducir siempre 
+						if(!paraPlataforma){
+							paraPlataforma=true;
+							StartCoroutine(paraDelay());
+						}
+						
 					}
 				}
 				if(eix_Y){	
-					if (y<PosicioFinalY){
-						paraPlataforma=true;
-						StartCoroutine(paraDelay());
-						direccio = !this.direccio;
+					if (y < PosicioFinalY){
+						//Debug.Log("ENTRA Y < P");
+						direccio = true; //para añadir siempre y subir
+						if(!paraPlataforma){
+							paraPlataforma=true;
+							StartCoroutine(paraDelay());
+							//Debug.Log("SALE Y < P");
+						}
 					}
-					if (y>PosicioInicialY){
-						paraPlataforma=true;
-						StartCoroutine(paraDelay());
-						direccio = !this.direccio;
+					if (y > PosicioInicialY){
+						//Debug.Log("ENTRA Y > P");
+						direccio = false; //para reducir siempre y bajar
+						if(!paraPlataforma){
+							paraPlataforma=true;
+							StartCoroutine(paraDelay());
+							//Debug.Log("SALE Y > P");
+						}
 					}
 				}
 				if(eix_Z){
 					if (z<PosicioFinalZ){
-						paraPlataforma=true;
-						StartCoroutine(paraDelay());
-						direccio = !this.direccio;
+						direccio = true; //para añadir siempre 
+						if(!paraPlataforma){
+							paraPlataforma=true;
+							StartCoroutine(paraDelay());
+						}
 					}
 					if (z>PosicioInicialZ){
-						paraPlataforma=true;
-						StartCoroutine(paraDelay());
-						direccio = !this.direccio;
+						direccio = false; //para reducir siempre y bajar
+						if(!paraPlataforma){
+							paraPlataforma=true;
+							StartCoroutine(paraDelay());
+						}
 					}
 				}
-					
-			}else{
+				
+				/* DEBUG MODE
+				if(botoActivat)
+					debugBoton = "BOTON ON";
+				else
+					debugBoton = "BOTON OFF";
+			
+				if(direccio)
+					debugDireccion = "DIR ON";
+				else
+					debugDireccion = "DIR OFF";
+			
+				if(paraPlataforma)
+					Debug.Log("POS INICIAL Y"+PosicioInicialY+" X -> "+x+"Y -> "+y+"Z -> "+z+" PLAT: OFF |" + debugBoton + "DIRECC "+debugDireccion);
+				else
+					Debug.Log("POS INICIAL Y"+PosicioInicialY+" X -> "+x+"Y -> "+y+"Z -> "+z+" PLAT: ON |" + debugBoton + "DIRECC "+debugDireccion );
+				*/
+				
+			}
+			else{
 				this.transform.Translate(0, 0, 0);
 			}
-				
+			
 			
 			
 		}
-		
-		
-		
 	}
 		
 	IEnumerator paraDelay(){
-		yield return new WaitForSeconds(2);
-		paraPlataforma=false;
-		Debug.Log("despres");
-		
+		if(paraPlataforma){
+			yield return new WaitForSeconds(2);
+			if(paraPlataforma)
+				paraPlataforma=false;
+			//Debug.Log("ACTIVAR DE NUEVO LA PLATAFORMA");
+		}
+		else{
+			yield return new WaitForSeconds(0); 
+			//Debug.Log("ACTIVAR DE NUEVO LA PLATAFORMA HACK");
+		}
 	}
 		
 		
@@ -179,7 +185,7 @@ public class codiPlataformaCs : MonoBehaviour {
 		//transform.Translate(0, -translation, 0);
 	}*/
 	
-	
+	/* useless code now
 	void OnTriggerStay(Collider other){
 		
             if(other.gameObject == player){
@@ -188,11 +194,11 @@ public class codiPlataformaCs : MonoBehaviour {
                         
             }
         }
+	*/
 	
 	void setBotoActivat(){
-		Debug.Log("he activat boto");
+		//Debug.Log("he activat boto");
 		botoActivat = !this.botoActivat;
-		//mourePlataforma();
 	}
 	
 	void OnCollisionEnter(Collision Target){  
