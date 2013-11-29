4747f6747eb75fa8ed4f4b51062ef672d9e2ec75
Alexander Avila <alex24.bcn@gmail.com>
2013-11-29 20:23:07 +0100

    Arreglar pantalla creditos Issue #342

--- Badass_Upgrade/UNITY/Assets/Scripts/GrupoE/creditsControl.cs
+++ Badass_Upgrade/UNITY/Assets/Scripts/GrupoE/creditsControl.cs
@@ -7,17 +7,17 @@ public class creditsControl : MonoBehaviour {
 	const int main_menu = 0;
 
 	public float t;
-	public TextMesh text;
+	//public TextMesh text;
 	public Vector3 origen;
 	public Vector3 destino;
 	
 	// Use this for initialization
 	void Start () {
 		Time.timeScale = 1;
-		origen = new Vector3(0,6,-10);
-		destino = new Vector3(0,-108,-10);
+		origen = new Vector3(0,1,-10);
+		destino = new Vector3(0,-100,-10);
 		Camera.main.gameObject.transform.position = origen;
-		text.text = "A nuestros familiares, amigos y a todos los que han\ncolaborado en la realización de este proyecto.";
+		//text.text = "A nuestros familiares, amigos y a todos los que han\ncolaborado en la realización de este proyecto.";
 	}
 	
 	// Update is called once per frame
