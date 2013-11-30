1ada73b1f70aa51bd14671e29efe34e9729e1e24
D. Corto <daviscorto@gmail.com>
2013-11-30 15:10:49 +0100

    Añadido el daño a Barril en el bullet (Issue #389)

--- Badass_Upgrade/UNITY/Assets/Scripts/GrupoA/Player/bullet.cs
+++ Badass_Upgrade/UNITY/Assets/Scripts/GrupoA/Player/bullet.cs
@@ -16,12 +16,15 @@ public class bullet : MonoBehaviour {
 	}
 	
 	void OnTriggerEnter(Collider other) {
-		Debug.Log("Toco "+other.collider.gameObject.tag);
+		Debug.Log("Toco con el bullet -> "+other.collider.gameObject.tag);
 		if(other.collider.gameObject.tag == "Enemy") {
 			other.transform.gameObject.SendMessage("rebreDany",damage);
 			Debug.Log("Damage enemic = "+damage);
 			Destroy(this.gameObject);
 		}
+		else if(other.collider.gameObject.tag == "Barril") {			
+		    other.transform.gameObject.SendMessage("rebreTir");
+		}		
 		else if(other.collider.gameObject.tag != "Enemy" && (other.collider.gameObject.tag != "Player") && (other.collider.gameObject.tag != "muzzleFlash")) {
 			Destroy(this.gameObject);
 		}
