f987edafa486a7eca820b138b725f668b41ccd12
Dabami <dabami7@gmail.com>
2013-12-01 16:51:33 +0100

    #407 Daño de los barriles
    Los barriles hacen un daño inversamente proporcional a la distancia con
    el jugador. @DCorto, está preparado con unas constantes que se pueden
    cambiar al gusto para calibrar el daño.

--- Badass_Upgrade/UNITY/Assets/Scripts/ProvesBarril.cs
+++ Badass_Upgrade/UNITY/Assets/Scripts/ProvesBarril.cs
@@ -3,16 +3,27 @@ using System.Collections;
 
 public class ProvesBarril : MonoBehaviour {
 	
-	int vida = 3;
+	const int MULTIPLICADOR = 20;
+	const float DISTANCIA_MAXIMA = 15;
+	
+	int vida;
+	
+	float distance;
+	int dany;
 	public GameObject Barril;
 	public GameObject Destroy;
 	public GameObject Fire;
+	public GameObject Player;
 	
 	// Use this for initialization
 	void Start () {
 		Destroy.SetActive(false);
 		Fire.SetActive(false);
 		//Barril.SetActive(false);
+		
+		vida = 3;
+		distance = 0;
+		dany = 0;
 	}
 	
 	// Update is called once per frame
@@ -30,6 +41,11 @@ public class ProvesBarril : MonoBehaviour {
 		}
 		if(vida <= 0) {
 			Barril.SetActive(false);
+			distance = Vector3.Distance (Barril.transform.position, Player.transform.position);
+			if(distance <= DISTANCIA_MAXIMA){
+				dany = (int)(DISTANCIA_MAXIMA/distance)*MULTIPLICADOR;
+				Player.SendMessage("rebreAtac",dany);
+			}
 			Destroy.SetActive(true);
 			
 		}
