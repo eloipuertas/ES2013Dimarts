47aa38dcf24eebae023ed246adb05fd0d47e73fc
AlcoholNahMas <cucaverda@hotmail.com>
2013-12-03 21:34:56 +0100

    #433 laser torreta faci mal
    Al pasar pel laser vermell fa mal. Sa de posar el script
    "CodiLaserVermell" al objecte "cubDeteccio" que esta dintre del laser.
    Aixo crida a la funcio "rebreAtac" de player. Es pot posar el valor de
    dany que vulguis desde UNity.

---
+++ Badass_Upgrade/UNITY/Assets/Scripts/CodiLaserVermell.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CodiLaserVermell : MonoBehaviour {
+	
+	public GameObject Player;
+	public int valorDany;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnTriggerStay(Collider other){
+		Debug.Log("Dintre laser vermell");
+		if(other.CompareTag("Player")){ 
+	
+        	Player.gameObject.SendMessage("rebreAtac", valorDany);
+        }
+	}
+}
