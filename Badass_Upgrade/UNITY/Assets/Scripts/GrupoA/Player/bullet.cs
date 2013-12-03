e723c4b8d45b3f5d668404ddc9bba604d19dda1e
D. Corto <daviscorto@gmail.com>
2013-12-03 23:59:35 +0100

    Fix del Barril para detectar la colisión con el bullet. (Issue #346)
    Simplemente activando las opciones: Convex y Smooth Sphere Collisions
    dentro del Mesh Collider del objeto "barrilCorrecte" de los barriles.

--- Badass_Upgrade/UNITY/Assets/Scripts/GrupoA/Player/bullet.cs
+++ Badass_Upgrade/UNITY/Assets/Scripts/GrupoA/Player/bullet.cs
@@ -16,13 +16,14 @@ public class bullet : MonoBehaviour {
 	}
 	
 	void OnTriggerEnter(Collider other) {
-		Debug.Log("Toco con el bullet -> "+other.collider.gameObject.tag);
+		//Debug.Log("Toco con el bullet -> "+other.collider.gameObject.name);
 		if(other.collider.gameObject.tag == "Enemy") {
 			other.transform.gameObject.SendMessage("rebreDany",damage);
-			Debug.Log("Damage enemic = "+damage);
+			//Debug.Log("Damage enemic = "+damage);
 			Destroy(this.gameObject);
 		}
 		else if(other.collider.gameObject.tag == "Barril") {			
+			//Debug.Log("TOCO AL BARRIL ->"+other.name);
 		    other.transform.gameObject.SendMessage("rebreTir");
 		}		
 		else if(other.collider.gameObject.tag != "Enemy" && (other.collider.gameObject.tag != "Player") && (other.collider.gameObject.tag != "muzzleFlash")) {
